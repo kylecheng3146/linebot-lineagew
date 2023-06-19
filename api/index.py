11358d61488db725f186c06df5359ab18b939037
Kyle Cheng <kylecheng3146@gmail.com>
2023-06-19 10:09:06 +0800

    fix: remove cheatgpt import

--- api/index.py
+++ api/index.py
@@ -2,7 +2,6 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from api.chatgpt import ChatGPT
 
 import os
 
