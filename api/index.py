0e284e19b37790f25d1762411d494d578e57716c
Kyle Cheng <kylecheng3146@gmail.com>
2023-06-20 08:53:46 +0800

    chore: 新增註解

--- api/index.py
+++ api/index.py
@@ -46,99 +46,135 @@ def handle_message(event):
     conn = connect_to_db()
     cursor = conn.cursor()
 
+    # 如果關鍵字為 "簽到"
     if keywords == "簽到":
+        # 從訊息中取得用戶的資訊
         line_name = parts[1]
         lineagew_name = parts[2]
         club = parts[3]
         try:
+            # 將用戶的資訊插入到資料庫中
             query = "INSERT INTO member (lineagew_name, line_name, club) VALUES (%s, %s, %s)"
             data = (lineagew_name, line_name, club)
             cursor.execute(query, data)
+            # 提交插入操作
             conn.commit()
+            # 回覆簽到成功訊息
             reply_msg = lineagew_name + " 簽到成功"
         except (Exception, psycopg2.Error) as error:
+            # 如果插入過程中出現錯誤，則回覆簽到失敗訊息
             reply_msg = lineagew_name + " 簽到失敗"
         finally:
+            # 最後，關閉資料庫連接
             conn.close()
                 
+        # 透過 Line Bot API 回覆訊息
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
         return
 
+    # 如果關鍵字為 "修改"
     if keywords == "修改":
+        # 從訊息中取得舊的資訊
         old_line_name = parts[1]
         old_lineagew_name = parts[2]
         old_club = parts[3]
+        # 從訊息中取得新的資訊
         line_name = parts[4]
         lineagew_name = parts[5]
         club = parts[6]
         try:
+            # 更新資料庫中的資訊
             query = "UPDATE member SET lineagew_name = %s, line_name = %s, club = %s WHERE lineagew_name = %s OR line_name = %s OR club = %s"
             data = (lineagew_name, line_name, club, old_lineagew_name, old_line_name, old_club)
             cursor.execute(query, data)
+            # 提交更新操作
             conn.commit()
+            # 回覆更新成功訊息
             reply_msg = old_lineagew_name + " 修改為 " + lineagew_name + "成功"
         except (Exception, psycopg2.Error) as error:
+            # 如果更新過程中出現錯誤，則回覆更新失敗訊息
             reply_msg = old_lineagew_name + " 修改為 " + lineagew_name + "失敗"
         finally:
+            # 最後，關閉資料庫連接
             conn.close()
                 
+        # 透過 Line Bot API 回覆訊息
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
         return
 
+    # 如果關鍵字為 "刪除"
     if keywords == "刪除":
+        # 從訊息中取得要刪除的關鍵字
         keyword = parts[1]
         try:
+            # 如果關鍵字為 "ALL"，則刪除所有成員
             if keyword == "ALL":
                 query = "DELETE FROM member"
                 cursor.execute(query)
             else:
+                # 否則，根據關鍵字刪除特定成員
                 query = "DELETE FROM member WHERE lineagew_name = %s OR line_name = %s"
                 data = (keyword,keyword)
                 cursor.execute(query, data)
 
+            # 提交刪除操作
             conn.commit()
+            # 回覆刪除成功訊息
             reply_msg = keyword + " 刪除成功"
         except (Exception, psycopg2.Error) as error:
+            # 如果刪除過程中出現錯誤，則回覆刪除失敗訊息
             reply_msg = keyword + " 刪除失敗"
         finally:
+            # 最後，關閉資料庫連接
             conn.close()
                 
+        # 透過 Line Bot API 回覆訊息
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
         return
 
+    # 如果關鍵字為 "找"
     if keywords == "找":
+        # 從訊息中取得查詢的關鍵字
         keyword = parts[1]
+        # 連接到資料庫
         conn = connect_to_db()
         cursor = conn.cursor()
         try:
-
+            # 如果關鍵字為空，則查詢所有成員
             if keyword == "":
                 query = "SELECT * FROM member"
                 cursor.execute(query)
             else:
+                # 否則，根據關鍵字查詢成員
                 query = "SELECT * FROM member WHERE lineagew_name LIKE %s OR line_name LIKE %s OR club LIKE %s"
                 cursor.execute(query, (f'%{keyword}%', f'%{keyword}%', f'%{keyword}%'))
 
+            # 獲取查詢結果
             results = cursor.fetchall()
 
+            # 格式化查詢結果
             formatted_results = "==== 查詢結果 ====\n"
             for row in results:
                 formatted_row = " - ".join(str(item) for item in row)
                 formatted_results += f"{formatted_row}\n"
             formatted_results += "================"
+
+            # 透過 Line Bot API 回覆訊息
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=formatted_results))
             return 
         except (Exception, psycopg2.Error) as error:
+            # 如果查詢過程中出現錯誤，則輸出錯誤訊息
             print("查询資料出錯:", error)
         finally:
+            # 最後，關閉資料庫連接
             conn.close()
         return
 
